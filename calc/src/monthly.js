ae3f117b8c84daa78fb60b71c690a2a07769b73b
JR <jfrazier@tiggee.com>
2018-07-14 10:10:56 -0400

    fixed NaN issue in Monthly

--- calc/src/monthly.js
+++ calc/src/monthly.js
@@ -3,7 +3,11 @@ import DnsCalc from "./dns-calc.js";
 
 class Monthly extends React.Component {
   render() {
-    return <h2 className={this.props.className}> {this.props.obj.records} </h2>;
+    return (
+      <h2 className={this.props.className}>
+        {this.props.obj.records ? this.props.obj.records : 0}
+      </h2>
+    );
   }
 }
 
