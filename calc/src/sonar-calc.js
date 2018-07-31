3330e3ee001f08b7578c34f201bd5b5e9160c28f
JR <jfrazier@tiggee.com>
2018-07-31 11:49:11 -0400

    cleaned up code in sonar-calc

--- calc/src/sonar-calc.js
+++ calc/src/sonar-calc.js
@@ -3,41 +3,6 @@ import SonarChecks from "./sonar-checks.js";
 
 
 class SonarCalc extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      http_locations: {
-        na_eu: 0,
-        ap: 0,
-        oc: 0
-      },
-      https_locations: {
-        na_eu: 0,
-        ap: 0,
-        oc: 0
-
-      },
-      ftp_locations: {
-        na_eu: 0,
-        ap: 0,
-        oc: 0
-      },
-      tcp_locations: {
-        na_eu: 0,
-        ap: 0,
-        oc: 0
-      },
-      dns_locations: {
-        na_eu: 0,
-        ap: 0,
-        oc: 0  
-      }
-    }
-    }
-
-    saveLocation(i) {
-      console.log(i);
-    }
 
   render() {
     return  (
